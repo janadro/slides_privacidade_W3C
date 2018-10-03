3ad4b562b2f0185cbe9a303660659659c3655cb2
janainerodrigues@alunos.utfpr.edu.br <janainerodrigues@alunos.utfpr.edu.br>
2018-10-03 12:40:27 -0300

    slides em construcao

---
+++ markup.js
@@ -0,0 +1,9 @@
+<div class="reveal">
+	<div class="slides">
+		<section>Slide Horizontal Simples</section>
+		<section>
+			<section>Slide Vertical 1 </section>
+			<section>Slide Vertical 2 </section>
+		</section>
+	</div>
+</div>
